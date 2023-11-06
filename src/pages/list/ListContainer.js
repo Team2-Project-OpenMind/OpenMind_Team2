e8b906e815f1a6c8aff4a6aeefcb26df99c3c242
namminimi <tbvhdaos@naver.com>
2023-11-06 21:00:09 +0900

    add: listPage 퍼블리싱(반응형)

--- src/pages/list/ListContainer.js
+++ src/pages/list/ListContainer.js
@@ -1,20 +1,26 @@
+import CardList from 'components/listPageCard/CardList';
+import * as style from './ListStyle';
+
+const numberList = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
 export default function ListContainer() {
   return (
-    <div>
-      <div>
-        <h2>누구에게 질문할까요?</h2>
-        <select>
-          <option>이름순</option>
-          <option>최신순</option>
-        </select>
-        <ul>
-          <li>
-            <img src="/images/profile.svg" />
-            <h4>아초는 고양이</h4>
-            <div></div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <style.ListContainerBox>
+      <style.ListWrap>
+        <style.ListUpper>
+          <style.ListTitle>누구에게 질문할까요?</style.ListTitle>
+          <style.ListSelect>
+            <style.ListOption>이름순</style.ListOption>
+            <style.ListOption>최신순</style.ListOption>
+          </style.ListSelect>
+        </style.ListUpper>
+        <CardList />
+        <style.ListPagination>
+          {numberList.map((list, index) => {
+            return <style.ListPaginationNumber key={index}>{list}</style.ListPaginationNumber>;
+          })}
+        </style.ListPagination>
+      </style.ListWrap>
+    </style.ListContainerBox>
   );
 }
