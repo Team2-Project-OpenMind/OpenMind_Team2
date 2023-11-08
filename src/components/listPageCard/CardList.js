9c651d86a74dcde34a0f82b75e57ccd2f4bced69
namminimi <tbvhdaos@naver.com>
2023-11-08 23:24:02 +0900

    feat: 질문 목록 리스트 이름순, 최신순 정렬

--- src/components/listPageCard/CardList.js
+++ src/components/listPageCard/CardList.js
@@ -4,27 +4,32 @@ import * as S from './CardListStyle';
 import { useState } from 'react';
 import Pagination from 'components/pagination/Pagination';
 
-export default function CardList({ data, message }) {
+export default function CardList({ data, message, range }) {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostPerPage] = useState(8); //한페이지당 렌더링 되는 데이터 수
 
   //페이지숫자 리스트 구현 계산
-  const indexOfLast = currentPage * postsPerPage; //페이지 마지막수 1 * 10
-  const indexOfFirst = indexOfLast - postsPerPage; // 페이지 첫번째 수10 - 10 = 0
+  const indexOfLast = currentPage * postsPerPage; //페이지 마지막수 1 * 8
+  const indexOfFirst = indexOfLast - postsPerPage; // 페이지 첫번째 수8 - 8 = 0
 
   const currentPosts = (datas) => {
-    let currentPosts = datas.slice(indexOfFirst, indexOfLast); //데이터를 0~10번째까지 슬라이스함
+    let currentPosts = datas.slice(indexOfFirst, indexOfLast); //데이터를 0~8번째까지 슬라이스함 인덱스0~7까지
     return currentPosts;
   };
 
   if (!data) return;
 
   const { results } = data;
-  console.log(results);
-  const sortData = results.sort((a, b) => (a.name > b.name ? 1 : -1));
+  const sortData = results.sort((a, b) => {
+    if (range === '이름순') {
+      return a.name > b.name ? 1 : -1;
+    } else {
+      return a.createdAt > b.createdAt ? -1 : 1;
+    }
+  });
 
   const postLists = currentPosts(sortData); //위 조건문 통과 후에 페이징 슬라이스
-  console.log(postLists);
+
   return (
     <>
       <S.ListCards>
