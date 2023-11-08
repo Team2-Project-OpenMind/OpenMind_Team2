0fd769b5c97bd1166addb2223f317896b7877ea0
ParkSohyunee <sohyunee016@gmail.com>
2023-11-08 17:33:15 +0900

    Fix(CardItem): 스타일 props Prefix 수정

--- src/components/AnswerStateTag.js
+++ src/components/AnswerStateTag.js
@@ -3,18 +3,17 @@ import styled from 'styled-components';
 export default function AnswerStateTag({ state }) {
   const text = state ? '답변 완료' : '미답변';
 
-  return <Tag state={state}>{text}</Tag>;
+  return <Tag $stateColor={state}>{text}</Tag>;
 }
 
 export const Tag = styled.span`
-  width: ${({ $state }) => ($state ? '8rem' : '6.5rem')};
+  width: ${({ $stateColor }) => ($stateColor ? '8rem' : '6.5rem')};
   padding: 0.4rem 1.2rem;
   font-size: 1.4rem;
   font: var(--caption1-regular);
-  color: var(--brown40);
-  color: ${({ state }) => (state ? 'var(--brown40)' : 'var(--gray40);')};
+  color: ${({ $stateColor }) => ($stateColor ? 'var(--brown40)' : 'var(--gray40)')};
   text-align: center;
-  border: 1px solid var(--brown-40, #542f1a);
+  border: 1px solid ${({ $stateColor }) => ($stateColor ? 'var(--brown40)' : 'var(--gray40)')};
   border-radius: 0.8rem;
   background: var(--grayscale-10, #fff);
 `;
