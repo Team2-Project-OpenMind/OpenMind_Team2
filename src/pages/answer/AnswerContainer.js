a696f82feb090a1f84d4b3c616ae699874ce1911
고민혁 <yeppi9801@gmail.com>
2023-11-10 17:00:23 +0900

    Feat(AnswerStyle.js): 답변 페이지 스타일 분리 및 post페이지와 구분되는 고유 컴포넌트 추가

--- src/pages/answer/AnswerContainer.js
+++ src/pages/answer/AnswerContainer.js
@@ -1,5 +1,5 @@
 import * as S from '../post/PostStyle';
-
+import { DeleteButton, ButtonWrapper } from './AnswerStyle.js';
 import { useState, useEffect } from 'react';
 import ClipBoardCopyMessage from 'components/ClipBoardCopyMessage';
 import FeedCard from 'components/answerFeedCard/FeedCard.js';
@@ -12,7 +12,6 @@ import FACEBOOK from 'assets/images/ShareIcon_FACEBOOK.svg';
 
 export default function Answer({ userId }) {
   const [questionList, setQuestionList] = useState([]);
-  const [isOpenModal, setOpenModal] = useState(false);
   const [answererProfile, setAnswererProfile] = useState({});
 
   const handleRenderSubjectsOnQ = async (id) => {
@@ -34,10 +33,6 @@ export default function Answer({ userId }) {
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const handleClickButton = () => {
-    setOpenModal(!isOpenModal);
   };
 
   const handleAllDeleteQuestionList = async (id) => {
@@ -71,11 +66,9 @@ export default function Answer({ userId }) {
           <S.LinkIcon src={FACEBOOK} alt="페이스북링크_아이콘"></S.LinkIcon>
         </S.LinkContainer>
 
-        <S.ButtonWrapper>
-          <S.DeleteButton onClick={() => handleAllDeleteQuestionList(userId)}>
-            삭제하기
-          </S.DeleteButton>
-        </S.ButtonWrapper>
+        <ButtonWrapper>
+          <DeleteButton onClick={() => handleAllDeleteQuestionList(userId)}>삭제하기</DeleteButton>
+        </ButtonWrapper>
         <S.FeedContainer>
           <S.Info>
             <S.IconMessage />
